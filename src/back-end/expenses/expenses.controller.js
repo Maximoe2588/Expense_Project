cf07544c33bdf11326297b4965aff24c8f824b0e
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-05-24 22:44:27 -0500

    update controller file

--- src/back-end/expenses/expenses.controller.js
+++ src/back-end/expenses/expenses.controller.js
@@ -1,1 +1,63 @@
-module.exports = 
+const knex = require('knex'); 
+const db = knex(); 
+
+const getAllExpenses = async (req, res) => {
+  try {
+    const expenses = await db.select().from('expenses'); 
+    res.json(expenses);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error retrieving expenses' });
+  }
+};
+
+const createExpense = async (req, res) => {
+  const { amount, description, category, date } = req.body;
+  try {
+    const newExpense = await db('expenses').insert({
+      amount,
+      description,
+      category,
+      date,
+    }); 
+    res.json({ id: newExpense[0], message: 'Expense created successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error creating expense' });
+  }
+};
+
+const updateExpense = async (req, res) => {
+  const { id } = req.params;
+  const { amount, description, category, date } = req.body;
+  try {
+    await db('expenses').where({ id }).update({
+      amount,
+      description,
+      category,
+      date,
+    }); 
+    res.json({ message: 'Expense updated successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error updating expense' });
+  }
+};
+
+const deleteExpense = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await db('expenses').where({ id }).del(); 
+    res.json({ message: 'Expense deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error deleting expense' });
+  }
+};
+
+module.exports = {
+  getAllExpenses,
+  createExpense,
+  updateExpense,
+  deleteExpense,
+};
